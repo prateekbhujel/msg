300a05569bdf86b5c9e3ae89641e13a59cfc2da8
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-05-28 16:11:04 +0545

    [7.]#1 --'Profile Section making it dynamic..'

---
+++ resources/views/messenger/layouts/profile-modal.blade.php
@@ -0,0 +1,38 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+        <div class="modal-body">
+            <form action="#">
+
+                <div class="file">
+                    <img src="{{ asset(auth()->user()->avatar) }}" alt="Upload" class="img-fluid">
+                    <label for="select_file"><i class="fal fa-camera-alt"></i></label>
+                    <input id="select_file" type="file" hidden>
+                </div>
+                <p>Edit information</p>
+                <input type="text" placeholder="Name" value="{{ auth()->user()->name }}">
+                <input type="text" placeholder="User Name" value="{{ auth()->user()->user_name }}">
+                <input type="email" placeholder="Email" value="{{ auth()->user()->email }}">
+                <p>Change password</p>
+                <div class="row">
+                    <div class="col-xl-6">
+                        <input type="password" placeholder="Old Password">
+                    </div>
+                    <div class="col-xl-6">
+                        <input type="password" placeholder="New Password">
+                    </div>
+                    <div class="col-xl-12">
+                        <input type="password" placeholder="Confirm Password">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary cancel"
+                data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary save">Save changes</button>
+        </div>
+    </div>
+</div>
+</div>
--- resources/views/messenger/layouts/user-list-sidebar.blade.php
+++ resources/views/messenger/layouts/user-list-sidebar.blade.php
@@ -7,45 +7,7 @@
         <span class="setting" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <i class="fas fa-user-cog"></i>
         </span>
-
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <form action="#">
-
-                            <div class="file">
-                                <img src="images/upload_img.jpg" alt="Upload" class="img-fluid">
-                                <label for="select_file"><i class="fal fa-camera-alt"></i></label>
-                                <input id="select_file" type="file" hidden>
-                            </div>
-                            <p>Edit information</p>
-                            <input type="text" placeholder="Name">
-                            <input type="email" placeholder="Email">
-                            <input type="text" placeholder="Phone">
-                            <p>Change password</p>
-                            <div class="row">
-                                <div class="col-xl-6">
-                                    <input type="password" placeholder="Old Password">
-                                </div>
-                                <div class="col-xl-6">
-                                    <input type="password" placeholder="New Password">
-                                </div>
-                                <div class="col-xl-12">
-                                    <input type="password" placeholder="Confirm Password">
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary cancel"
-                            data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary save">Save changes</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('messenger.layouts.profile-modal')
     </div>
 
     <form action="#" class="wsus__user_list_search">

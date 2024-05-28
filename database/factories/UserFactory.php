83294180a21d19636572fb0fdfb972fdbc9a01df
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-05-28 15:57:11 +0545

    [6.]#2 --'User Factory Data migrated and created.'

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'user_name' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
